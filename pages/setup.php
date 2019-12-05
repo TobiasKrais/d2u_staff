e0a505003ca281c52c261765ae416b1d5e554e12
Tobias Krais <tk@design-to-use.de>
2019-12-05 17:12:27 +0100

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -28,6 +28,10 @@ $d2u_module_manager->showManagerList();
 <p>Fehlermeldungen bitte über
 	<a href="https://github.com/TobiasKrais/d2u_staff/issues" target="_blank">GitHub</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.0.4:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.0.3:</p>
 <ul>
 	<li>Backend: Einstellungen und Setup Tabs rechts eingeordnet um sie vom Inhalt besser zu unterscheiden.</li>
